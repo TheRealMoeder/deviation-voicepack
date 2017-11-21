ed761e94a9213a1ee99b3dc8a6c17f8d1d1a8b1d
TheRealMoeder <canofspam@web.de>
2017-11-21 21:24:33 +0100

    Create new index.md for voicepacker builds

--- create_index.sh
+++ create_index.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+echo '---' > index.md
+echo 'layout: default' >> index.md
+echo '---' >> index.md
+echo '### Downloads' >> index.md
+
 for INFILE in *.zip
 do
 	echo "- [$INFILE](${INFILE})" >> index.md
 done
+
 echo $'\n'
 echo "* * *" >> index.md
 echo "Last updated:" >> index.md
