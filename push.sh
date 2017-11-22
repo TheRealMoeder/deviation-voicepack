78d339327c9c5ccdc7c9c47ab90d6e4d1455a7e1
TheRealMoeder <canofspam@web.de>
2017-11-22 06:47:23 +0100

    fix push

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 setup_git() {
   cd
@@ -17,10 +17,12 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
   git push --quiet
 }
 
+echo "Setting up git..."
 setup_git
+echo "Committing files..."
 commit_website_files
+echo "Pushing to remote..."
 upload_files
