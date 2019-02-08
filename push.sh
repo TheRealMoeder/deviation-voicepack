6dd4397fb4d86a97aeb90dd8a9b94a46cfb0bd3d
TheRealMoeder <canofspam@web.de>
2019-02-08 22:28:25 +0100

    only keep the last 30 builds on GitHub.io pages

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ setup_git() {
   cd
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  git clone --branch master https://${GITHUB_TOKEN}@github.com/TheRealMoeder/TheRealMoeder.github.io.git build/TheRealMoeder/TheRealMoeder.github.io > /dev/null 2>&1
+  git clone --depth=20 --branch master https://${GITHUB_TOKEN}@github.com/TheRealMoeder/TheRealMoeder.github.io.git build/TheRealMoeder/TheRealMoeder.github.io > /dev/null 2>&1
   cd build/TheRealMoeder/TheRealMoeder.github.io
 }
 
@@ -13,7 +13,9 @@ commit_website_files() {
   ../deviation-voicepack/create_index.sh
   git add index.md
   git add *.zip
-  git commit --message "$TRAVIS_COMMIT_MESSAGE"
+  git commit -m "Created build ID $BUILD_ID from voicepacker"
+  git remote add origin https://${GITHUB_TOKEN}@github.com/TheRealMoeder/TheRealMoeder.github.io.git
+  git push -u --force origin master
 }
 
 upload_files() {
@@ -21,14 +23,17 @@ upload_files() {
 }
 
 remove_old_files() {
-  find . -mtime +7 -exec git rm {} \;
+  for ((i=1; i<$BUILD_ID-30; i++)); do find -name $i-voice*.zip -delete; done
+  rm -rf .git
+  git init
+  git add .
 }
 
 echo "Setting up git..."
 setup_git
+echo "Removing old files and cleaning out repository..."
+remove_old_files
 echo "Committing files..."
 commit_website_files
-echo "Removing files older than one week..."
-remove_old_files
 echo "Pushing to remote..."
 upload_files
